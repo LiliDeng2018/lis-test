8763be7221a122964caa14f1e50be2280c8bf14d
Lili Deng (Wicresoft) <v-lide@microsoft.com>
2018-05-11 10:59:51 +0800

    update

--- WS2012R2/lisa/tools/middleware_bench/providers/amazon_service.py
+++ WS2012R2/lisa/tools/middleware_bench/providers/amazon_service.py
@@ -172,7 +172,7 @@ class AWSConnector:
         #
         filters={'name':'amzn-ami-hvm-*-x86_64-gp2', 'architecture': 'x86_64','root_device_type':'ebs', 'owner_id':'137112412989'}
         images = self.vpc_conn.get_all_images(filters=filters)
-        filters_images = None
+        filters_images = []
         for image in images:
             if image.platform != 'windows' and "test" not in image.name:
                 filters_images.add(image)
